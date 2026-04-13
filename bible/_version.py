5f88431307f28933f554ac404352dffdc33f2bb2
dxxbox <zhangxi.raul@gmail.com>
2026-04-13 17:51:40 +0800

    update pyproject.toml with ruff

--- bible/_version.py
+++ bible/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev37'
-__version_tuple__ = version_tuple = (0, 1, 'dev37')
+__version__ = version = '0.1.dev42'
+__version_tuple__ = version_tuple = (0, 1, 'dev42')
 
-__commit_id__ = commit_id = 'g9dd30940e'
+__commit_id__ = commit_id = 'g8f0387ec4'
