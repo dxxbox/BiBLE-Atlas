c361b4208c69d2ef16647fe08bc0dcc4eec5aff3
dxxbox <zhangxi.raul@gmail.com>
2026-06-01 21:17:57 +0800

    adding more dependencies.

--- bible/_version.py
+++ bible/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev114'
-__version_tuple__ = version_tuple = (0, 1, 'dev114')
+__version__ = version = '0.1.dev117'
+__version_tuple__ = version_tuple = (0, 1, 'dev117')
 
-__commit_id__ = commit_id = 'gd601cae80'
+__commit_id__ = commit_id = 'g6dcce2553'
